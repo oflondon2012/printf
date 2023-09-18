404dad0a3968d2736d6537f7c1bcf11cc03074ca
oflondon2012 <gtadebayo2023@gmail.com>
2023-09-18 23:09:21 +0000

    our first group project in alx

---
+++ _printf.c
@@ -0,0 +1,49 @@
+#include "main.h"
+/**
+ * _printf - function to print to std out
+ * @format: variable for format specifier
+ * Return: number of byte printed
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int i = 0, j = 0, counter = 0;
+
+	printer_t funcs[] = {{"c", print_c}, {"S", print_s}
+	};
+	va_start(args, format);
+	if (format == '\0')
+		return (-1);
+	while (format && format[i])
+	{
+		if (format[i] == '%')
+		{
+			if (format[i + 1 == '%')
+			{
+				_myputchar('%');
+				counter++;
+				i++;
+			}
+			else
+			{
+				i++;
+				j = 0;
+				while (funcs[j].format_spec)
+				{
+					if (format[i] == *(funcs[j].format_spec))
+					{
+						funcs[j].print(args);
+						counter++;
+						break;
+					}
+					j++;
+				}
+			}
+		}
+		else
+			_myputchar(format[i]);
+		i++;
+	}
+	va_end(args);
+	return (counter);
+}
