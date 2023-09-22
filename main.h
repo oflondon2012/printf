1dcaecde8063660146f0bb4e2ec4f3f3b0d15136
Donchocho1 <ogagaoghene11@gmail.com>
2023-09-21 17:32:46 -0700

    First Attempt

--- main.h
+++ main.h
@@ -6,24 +6,8 @@
 
 int _myputchar(char c);
 int putstr(char *ch);
-
-/**
- * struct printer- struct
- * @format_spec: identifier of type to print(e.g c means char)
- * @print: ptr ro functions that print according to identifier found
- */
-
-typedef struct printer
-{
-	char *format_spec;
-	void (*print)(va_list arg);
-} printer_t;
+int _recusive(int num1, int numb2);
 
 int _printf(const char *format, ...);
-void iformat(va_list args, char specifier);
-void istring(char *str);
-int ichar(int ch);
-
-
 
 #endif /* MAIN_H */
--- putstr.c
+++ putstr.c
@@ -9,14 +9,15 @@
 
 int putstr(char *ch)
 {
-	int j;
+	int j = 0, counter = 0;
 
 	if (ch)
 	{
 		for (j = 0; ch[j] != '\0'; j++)
 		{
 			_myputchar(ch[j]);
+			counter = counter + 1;
 		}
 	}
-	return (j);
+	return (counter);
 }
